584e63540bfb7589aa2ea23e63f633c5663e7c4f
alexgoldsmith <46906584+alexgoldsmith@users.noreply.github.com>
2021-11-04 17:22:50 -0500

    example api

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ app.get('/login', (req, res) => {
 	res.send({email: email, password: password});
 });
 
+app.get('/quizzes', (req, res) => {
+
+	// mock quiz object
+	const quiz =  {
+		title: "Fragility of Happiness Scale",
+		description: "How happy are you... really?",
+		questions: [
+			"Something might happen at any time and we could"
+			+ "easily lose our happiness",
+			"Happiness is fragile",
+			"it is likely that our happiness could be reduced to unhappiness"
+			+ "with a simple accident",
+			"There is only a thin line between happiness and unhappiness"
+		],
+		minPerQuestion: 0,
+		maxPerQuestion: 7,
+	}
+
+	/// set array size
+	const arraysize = 0;
+	
+	// create array to contain quiz objects
+	const quizObjects = [quiz];
+
+	// send array of quiz objects
+	res.send(quizObjects);
+})
+
 app.post('/login', (req, res) => {
 	res.send(req.body)
 });
