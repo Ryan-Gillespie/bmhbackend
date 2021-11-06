37867949ef0e1705fea8f68baee6a5e279472cf5
NPrime01 <47795760+NPrime01@users.noreply.github.com>
2021-11-06 18:52:03 -0500

    login working register in progress

---
+++ src/index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bp = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const { MongoClient } = require('mongodb');
+
+app.use(cors());
+app.use(bp.json());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+	const { MongoClient } = require('mongodb');
+	const uri = "mongodb+srv://admin:badgermentalhealth@cluster0.c61q2.mongodb.net/users?retryWrites=true&w=majority";
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+	client.connect(async err => {
+		const collection = client.db("users").collection("users");
+		res.send(await collection.findOne({email: req.headers.email}));
+		client.close();
+	});
+})
+
+app.get('/quizes', (req, res) => {
+
+	// mock quiz object
+	const quiz =  {
+		title: "Fragility of Happiness Scale",
+		description: "How happy are you... really?",
+		questions: [
+			"Something might happen at any time and we could"
+			+ "easily lose our happiness",
+			"Happiness is fragile",
+			"it is likely that our happiness could be reduced to unhappiness"
+			+ "with a simple accident",
+			"There is only a thin line between happiness and unhappiness"
+		],
+		minPerQuestion: 1,
+		maxPerQuestion: 7,
+		answerLegend:  ["Strongly disagree",
+						"Somewhat disagree",
+						"A little disagree",
+						"Neither Agree or Disagree",
+						"A little agree",
+						"Somewhat agree",
+						"Strongly agree"]
+	}
+
+	/// set array size
+	const arraysize = 0;
+	
+	// create array to contain quiz objects
+	const quizObjects = [quiz];
+
+	// send array of quiz objects
+	res.send(quizObjects);
+})
+
+app.get('/login', (req, res) => {
+	const {email, password} = req.headers
+
+	const uri = "mongodb+srv://admin:badgermentalhealth@cluster0.c61q2.mongodb.net/users?retryWrites=true&w=majority";
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+	client.connect(async err => {
+		const collection = client.db("users").collection("users");
+		const doc = await collection.findOne({email: email})
+		if (doc.password === password) {
+			res.send({token: doc._id})
+		}
+		else{
+			res.send({message: 'Wrong Password'})
+		}
+		client.close();
+	});
+});
+
+app.post('/login', (req, res) => {
+	const {email, password} = req.headers
+
+	const uri = "mongodb+srv://admin:badgermentalhealth@cluster0.c61q2.mongodb.net/users?retryWrites=true&w=majority";
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+	client.connect(async err => {
+		const collection = client.db("users").collection("users");
+		const doc = await collection.insertOne({email: email, password: password});
+		res.send(doc);
+		client.close();
+	});
+});
+
+app.listen(3001, () => {console.log("listening on port 3001")});
